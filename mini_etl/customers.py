231674e3bcb911266af802c856288f6b2c605b51
Gabi <g.stepien@k-tech.it>
2026-03-15 17:11:13 +0000

    clean up

--- mini_etl/customers.py
+++ mini_etl/customers.py
@@ -1,9 +1,6 @@
-import functools
-
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
+    Blueprint
 )
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from mini_etl.db import get_db
 
